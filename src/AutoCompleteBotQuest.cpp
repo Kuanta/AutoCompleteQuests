75d958b786e7e8827012266c9bd44671f77bb190
doruk-veloxia <erdemhan93@gmail.com>
2025-08-04 02:23:11 +0300

    Added a log for completing the quest

--- src/AutoCompleteBotQuest.cpp
+++ src/AutoCompleteBotQuest.cpp
@@ -2,6 +2,7 @@
 #include "Player.h"
 #include "Group.h"
 #include "QuestDef.h"
+#include "WorldSession.h"
 
 class AutoCompletePartyQuests : public PlayerScript
 {
@@ -13,6 +14,9 @@ public:
         if (!player->GetGroup())
             return;
 
+        // Oyuncunun hesap ID'si
+        uint32 masterAccountId = player->GetSession()->GetAccountId();
+
         Group *group = player->GetGroup();
         for (GroupReference *itr = group->GetFirstMember(); itr != nullptr; itr = itr->next())
         {
@@ -20,7 +24,11 @@ public:
             if (!member || member == player)
                 continue;
 
-            // Eğer ödül seçimi gerekiyorsa, atla
+            // Eğer başka bir hesaba aitse, dokunma
+            if (member->GetSession()->GetAccountId() != masterAccountId)
+                continue;
+
+            // Eğer quest'te ödül seçimi varsa, dokunma
             if (quest->GetRewChoiceItemsCount() > 1)
                 continue;
 
@@ -28,13 +36,18 @@ public:
             if (!member->GetQuestStatus(quest->GetQuestId()))
                 member->AddQuest(quest, nullptr);
 
-            // Eğer zaten tamamlanmışsa, atla
+            // Zaten bitmişse geç
             if (member->GetQuestStatus(quest->GetQuestId()) == QUEST_STATUS_COMPLETE)
                 continue;
 
-            // Bitmemişse, tamamlat ve ödülü ver
+            // Bitmemişse tamamlat
             member->CompleteQuest(quest->GetQuestId());
             member->RewardQuest(quest, false);
+
+            TC_LOG_INFO("server", "AutoComplete: %s completed quest %u (copied from %s)",
+                        member->GetName().c_str(),
+                        quest->GetQuestId(),
+                        player->GetName().c_str());
         }
     }
 };
